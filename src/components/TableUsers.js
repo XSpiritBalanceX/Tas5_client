ae97bd204bbf980f83058c33c9446c9a1e244c9b
Storm <lenagreidon@mail.ru>
2022-12-01 10:46:31 +0300

    done

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,8 +1,48 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useCallback} from 'react';
+import {Table} from 'react-bootstrap';
 
-const TableUsers=()=>{
+const TableUsers=(props)=>{
+    
+
+    const scrollPage = useCallback((event) => {
+          if ( event.target.documentElement.offsetHeight -(event.target.documentElement.scrollTop + window.innerHeight) < 100) {
+            props.setItem((prevState) => prevState + 10);
+          }
+          // eslint-disable-next-line
+        },[props.setItem]);
+    
+      useEffect(() => {
+        document.addEventListener("scroll", scrollPage);
+    
+        return () => document.removeEventListener("scroll", scrollPage);
+      }, [scrollPage]);
+
+    let oneUser=props.data&&props.data.slice(0, props.itemsInPage).map((el, ind)=>{
+        return( <tr key={ind}>
+            <td>{ind+1}</td>
+            <td>{el.id}</td>
+            <td>{el.name}</td>
+            <td>{el.address}</td>
+            <td>{el.phoneNumber}</td>
+        </tr>)
+    });
+
     return(
-        <div>TableUsers</div>
+        <div style={{marginTop:'3%'}}> 
+            <Table striped>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>ID</th>
+            <th>Full name</th>
+            <th>Address</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {oneUser}          
+        </tbody>
+      </Table></div>
     )
 }
 
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Navbar, Container, ButtonGroup , Button, Form} from 'react-bootstrap';
+import {Navbar, Container, ButtonGroup , Button, Form, Spinner} from 'react-bootstrap';
 import TableUsers from '../components/TableUsers';
 
 const MainPage=()=>{
@@ -7,28 +7,32 @@ const MainPage=()=>{
     const [country, setCountry]=useState('en');
     const [errInp, setErrInp]=useState(0);
     const [seed, setSeed]=useState(0);
-    const [page, setPage]=useState(0);
-    const [size, setSize]=useState(20/* 200 */);
+    const [testSeed, setTestSeed]=useState('');
     const [itemInPage, setItemInPage]=useState(20);
     const [fakeData, setFakeData]=useState([]);
     const [isLoad, setLoad]=useState(true);
+    const size=10;
+    const page=20;
 
-
-    const getFakeData=()=>{
-        fetch('http://localhost:5000/api/data'+`?seed=${seed}&page=${page}&size=${size}&err=${errInp}&local=${country}`)
-        .then(response=>response.json())
-        .then(data=>{console.log(data); setFakeData(data.users); setLoad(true)})
-        .catch(err=>console.log(err))
-    }
-
+//https://task5server-production.up.railway.app/api/data
     useEffect(()=>{
-        getFakeData();
+        fetch(`http://localhost:5000/api/data?seed=${seed}&page=${page}&size=${size}&err=${errInp}&local=${country}`)
+        .then(response=>response.json())
+        .then(data=>{setFakeData(data.users); setLoad(true)})
+        .catch(err=>console.log(err))
         // eslint-disable-next-line
-    },[country]);
+    },[country,seed,page,size, errInp]);
 
     const handleErr=(value)=>{
         let newErr=value>1000?1000:Number(value);
         setErrInp(newErr) 
+    }
+
+    const sendSeed=(event)=>{
+        if(event==='buttonRandom'){
+            setSeed(testSeed);
+        }
+        setTestSeed('');
     }
 
 
@@ -39,7 +43,8 @@ const MainPage=()=>{
                 <Navbar.Brand style={{color:'white'}}>Task 5</Navbar.Brand>
              </Container>
         </Navbar>
-        <div style={{margin:'2% 30% 2% 40%'}}>
+        <div style={{display:'flex', flexDirection:'row', margin:'0 5% 0 5%', justifyContent:'space-around', alignItems:'center'}}>
+        <div >
             <h4>Choose a country</h4>
         <ButtonGroup  className="mb-2" onClick={(event)=>{setCountry(event.target.name)}}>
         <Button variant="outline-dark" name={'en'}>USA</Button>
@@ -47,7 +52,6 @@ const MainPage=()=>{
         <Button variant="outline-dark" name={'fr'}>France</Button>
       </ButtonGroup>
         </div>
-        <div style={{display:'flex', flexDirection:'row', margin:'0 5% 0 5%', justifyContent:'space-around', alignItems:'center'}}>
          <div style={{width:'30%'}}>
             <Form.Label>Your range is {errInp}</Form.Label>
             <Form.Range step={0.5} min={0} max={10} value={errInp} onChange={(event)=>handleErr(event.target.value)}/>
@@ -55,11 +59,12 @@ const MainPage=()=>{
               value={errInp} onChange={(event)=> handleErr(event.target.value)} placeholder='Enter the number of errors'/>
          </div>
          <div style={{width:'40%', display:'flex', flexDirection:'row'}}>
-         <Form.Control style={{height:'2.5em'}} placeholder='Enter seed'/>
-         <Button variant="dark"  style={{height:'2.5em'}}>Random</Button>
+         <Form.Control style={{height:'2.5em'}} placeholder='Enter seed' value={testSeed} onChange={(event)=>{sendSeed(event.target.value); setTestSeed(event.target.value)}}/>
+         <Button variant="dark"  style={{height:'2.5em'}} name='buttonRandom' onClick={(event)=>sendSeed(event.target.name)}>Random</Button>
          </div>
         </div>
-        <TableUsers />
+        {isLoad?<TableUsers data={fakeData} itemsInPage={itemInPage} setItem={setItemInPage}/>:
+          <Spinner animation="border" variant="dark" style={{position:'absolute', top:'50%', left:'50%'}}/>}
         </div>
     )
 }
